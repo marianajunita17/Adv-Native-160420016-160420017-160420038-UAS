357ec86ce655993218575fc99a11b8b7e963f8f3
marianajunita17 <ymawuntu14@gmail.com>
2023-05-09 03:27:26 +0700

    second

--- app/src/main/java/com/mariana/adv160420017uts/view/MainActivity.kt
+++ app/src/main/java/com/mariana/adv160420017uts/view/MainActivity.kt
@@ -2,12 +2,27 @@ package com.mariana.adv160420017uts.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.mariana.adv160420017uts.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
     }
 }
