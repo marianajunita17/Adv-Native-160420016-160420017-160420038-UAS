7ddcebe2558cd887fa3b9e0922c51f82cd06bb76
ketketin <114962909+ketketin@users.noreply.github.com>
2023-07-06 20:11:38 +0700

    Sek baru dikit
    SABAR SBAR

--- app/src/main/java/com/mariana/adv160420017uts/util/TodoWorker.kt
+++ app/src/main/java/com/mariana/adv160420017uts/util/TodoWorker.kt
@@ -1,4 +1,14 @@
 package com.mariana.adv160420017uts.util
 
-class TodoWorker {
+import android.content.Context
+
+class TodoWorker(val context: Context, val params: WorkerParameters)
+    :Worker(context, params){
+        override fun doWork() : Result{
+            NotificationHelper(context)
+                .createNotification(inputData.getString("title").toString(),
+                                    inputData.getString("message").toString())
+
+            return Result.success()
+        }
 }
